87d92d8a65fa34e745cf98dff6f4b33902676403
chasscepts <francischassobetta@gmail.com>
2021-03-25 13:31:47 +0100

    add clear_lines method to improve display

--- bin/main.rb
+++ bin/main.rb
@@ -11,6 +11,12 @@ def clear_terminal
   system('clear') || system('cls') # https://stackoverflow.com/a/19059008
 end
 
+# https://stackoverflow.com/a/31090956
+# see comment by cremno
+def clear_lines(count)
+  count.times { print "\e[A\e[2K" }
+end
+
 def display_board
   puts '+---+---+---+'
   puts '| 1 | 2 | 3 |'
@@ -24,23 +30,25 @@ end
 def choose_winner(player1_name,player2_name)
   num = rand(15)
   if num < 5
-    puts "#{player1_name} wins"
+    puts "#{player1_name} you WIN the game"
   elsif num < 10
-    puts 'It is a tie'
+    puts "It's a TIE!"
+    puts
+    puts 'Game Over'
   else
-    puts "#{player2_name} wins"
+    puts "#{player2_name} you WIN the game"
   end
 end
 
 puts 'Welcome to Ruby Tic Tac Toe'
 puts
-puts 'Enter Player 1 Name'
-player1_name = user_input
+puts 'Enter Player 1 Name:'
+player1_name = user_input.capitalize
 
 puts
 
-puts 'Enter Player 2 Name'
-player2_name = user_input
+puts 'Enter Player 2 Name:'
+player2_name = user_input.capitalize
 
 puts
 puts "#{player1_name} will play #{CHARACTER_1}"
@@ -48,7 +56,7 @@ puts "#{player2_name} will play #{CHARACTER_2}"
 
 puts 'Let us play!'
 
-sleep(3)
+sleep(2)
 
 clear_terminal
 
@@ -57,20 +65,28 @@ sleep(1)
 display_board
 
 9.times do |turn|
+  lines = 6
+
   name = turn.even? ? player2_name : player1_name
+  puts
   puts "It's #{name}'s turn"
   puts
-  puts 'Please select an available cell from the board'
+  puts 'Please select an available cell from the board `- -`'
   puts
   cell = user_input.to_i
   while cell < 1 || cell > 9
-    puts 'Invalid move. Please enter integer from 1 to 9'
+    puts 'Please enter a number from 1-9'
     cell = user_input.to_i
+    lines += 2
   end
+  sleep 1
+  clear_lines(lines)
 end
 
 sleep(1)
 
-choose_winner(player1_name,player2_name)
+puts
+
+choose_winner(player1_name, player2_name)
 
 sleep(2)
