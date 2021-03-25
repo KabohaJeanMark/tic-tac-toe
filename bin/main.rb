13ead20bd4a36212a766560d32503bf0181d25fc
chasscepts <francischassobetta@gmail.com>
2021-03-25 11:10:57 +0100

    implement greeting and get player names

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,22 @@
 #!/usr/bin/env ruby
 
-def initial_method
-  puts 'Hello world!'
+CHARACTER_1 = 'X'.freeze
+CHARACTER_2 = 'O'.freeze
+
+def user_input
+  gets.chomp
 end
 
-initial_method
+puts 'Welcome to Ruby Tic Tac Toe'
+puts
+puts 'Enter Player 1 Name'
+player1_name = user_input
+
+puts
+
+puts 'Enter Player 2 Name'
+player2_name = user_input
+
+puts
+puts "#{player1_name} will play #{CHARACTER_1}"
+puts "#{player2_name} will play #{CHARACTER_2}"
